f041ceaf4ccd143ea060bfa0831794a8246d073b
wrj <wrj@qq.com>
2022-05-05 21:32:35 +0800

    user commit

--- src/main/java/com/example/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/example/reggie/filter/LoginCheckFilter.java
@@ -31,7 +31,9 @@ public class LoginCheckFilter implements Filter {
                 "/employee/logout",
                 "/backend/**",
                 "/front/**",
-                "/common/**"
+                "/common/**",
+                "/user/sendMsg",
+                "/user/login"
         };
         boolean check = check(urls, requestURI);
         //3.如果请求不需要处理，则放行
@@ -41,12 +43,22 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-        //4.判断用户有没有登录
+        //4-1.判断用户有没有登录
         if (request.getSession().getAttribute("employee") != null){
             log.info("用户已登录，用户id为:{}",request.getSession().getAttribute("employee"));
 
             Long empId = (Long) request.getSession().getAttribute("employee");
             BaseContext.setCurrentId(empId);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //4-2判断用户有没有登录
+        if (request.getSession().getAttribute("user") != null){
+            log.info("用户已登录，用户id为:{}",request.getSession().getAttribute("user"));
+
+            Long userId = (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
             filterChain.doFilter(request,response);
             return;
         }
